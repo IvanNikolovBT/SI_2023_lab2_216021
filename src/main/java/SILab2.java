d1a9bdce29e86bc2ab4c856d3eee5ad2128d64d6
Ivan Doma <102655208+IvanNikolovBT@users.noreply.github.com>
2023-05-31 19:41:39 +0200

    Initial commit od Intelij

--- src/main/java/SILab2.java
+++ src/main/java/SILab2.java
@@ -42,15 +42,15 @@ public class SILab2 {
         }
 
         int same = 1;
-        if (user.getEmail().contains("@") && user.getEmail().contains(".")) {
+        if (user.getEmail().contains("@") && user.getEmail().contains(".")) {  //proverka za validen email,mora da ima @ i tochka
             same = 0;
             for (int i=0;i<allUsers.size();i++) {
                 User existingUser = allUsers.get(i);
-                if (existingUser.getEmail() == user.getEmail()) {
-                    same += 1;
+                if (existingUser.getEmail() == user.getEmail()) { //od 49 do 52 treba , taka da razlichen email
+                    same += 1;   // ako ima ist imejl same zgolemi za eden
                 }
-                if (existingUser.getUsername() == user.getUsername()) {
-                    same += 1;
+                if (existingUser.getUsername() == user.getUsername()) {  // od 52 do 54, treba da ima raclichen userjenm
+                    same += 1;  // ako ima ist username zgolemi za eden
                 }
             }
         }
@@ -60,18 +60,17 @@ public class SILab2 {
         String passwordLower = password.toLowerCase();
 
         if (passwordLower.contains(user.getUsername().toLowerCase()) || password.length()<8) {
-            return false;
+            return false;  // ako e prisuten usernejmot vo passwordot ili ako pasvordot e pomalce od 8 vrati false
         }
-        else {
-            if (!passwordLower.contains(" ")) {
-                for (int i = 0; i < specialCharacters.length(); i++) {
-                    if (password.contains(String.valueOf(specialCharacters.charAt(i)))) {
-                        return same == 0;
+        else {  // ako e pravilen pasword
+            if (!passwordLower.contains(" ")) { //ako pasvordot nema prazni mesta
+                for (int i = 0; i < specialCharacters.length(); i++) {          //iterira na site specijalni karakteri
+                    if (password.contains(String.valueOf(specialCharacters.charAt(i)))) {      //
+                        return same == 0;   // ako nema nieden ist usernejm ili pasvord togash ke vrati true , mora da ima specijalen karakter
                     }
                 }
             }
-        }
-        return false;
+        }  // 66-74
+        return false;  // ovde e ako nema specijalen karakter i vrakja false
     }
-
-}
+}   // 66-74  znachi da ima pasvord sho ima prazno mesto
--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SILab2Test {
 
+
+    @Test
+    void testNaSite()
+    {
+
+    }
 }
